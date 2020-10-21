e76ba5daf50e968b962b3ef836b3ffd538f327fc
bharatagrawal <bharat.agrawal799@gmail.com>
2020-10-21 08:46:46 +0530

    Data Extraction for books spider

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -17,6 +17,24 @@ class BooksSpider(scrapy.Spider):
         next_page_url = response.xpath("//li[contains(@class,'next')]/a/@href").extract_first()
         next_page_url = response.urljoin(next_page_url)
         yield Request(next_page_url)
-        
+
     def parse_book(self,response):
-       	pass
+    	url = response.url
+    	book_name = response.xpath("//h1/text()").extract_first()
+    	price = response.xpath("//p[contains(@class,'price_color')]/text()").extract_first()
+
+    	image = response.xpath("//img/@src").extract_first()
+    	image = image.replace("../..","http://books.toscrape.com")
+    	
+    	rating = response.xpath("//p[contains(@class,'star-rating ')]/@class").extract_first()
+    	rating = rating.replace("star-rating ",'')
+
+    	descrption = response.xpath("//div[contains(@id,'product_description')]/following-sibling::p/text()").extract_first()
+    	yield{
+       		'Title': book_name,
+       		'Price': price,
+       		'URL' : url,
+       		'Image': image,
+       		'Descrption': descrption,
+       		'Rating':rating
+       	} 
