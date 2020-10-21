89abb0cab137e68aeb5c22d03e56fc498d29f120
bharatagrawal <bharat.agrawal799@gmail.com>
2020-10-21 09:39:59 +0530

    uploaded data in csv format

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,6 +1,9 @@
 import scrapy
 from scrapy.http import Request
 
+def product_info(response, value):
+	return response.xpath("//tr/th[contains(.,'"+value+"')]/following-sibling::td/text()").extract_first()
+
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
@@ -25,16 +28,31 @@ class BooksSpider(scrapy.Spider):
 
     	image = response.xpath("//img/@src").extract_first()
     	image = image.replace("../..","http://books.toscrape.com")
-    	
+
     	rating = response.xpath("//p[contains(@class,'star-rating ')]/@class").extract_first()
     	rating = rating.replace("star-rating ",'')
 
     	descrption = response.xpath("//div[contains(@id,'product_description')]/following-sibling::p/text()").extract_first()
+    	UPC = product_info(response,"UPC")
+    	Product_Type = product_info(response,"Product Type")
+    	Price_excl_tax = product_info(response,"Price (excl. tax)")
+    	Price_incl_tax = product_info(response,"Price (incl. tax)")
+    	Tax = product_info(response,"Tax")
+    	Availability = product_info(response,"Availability")
+    	Number_of_reviews = product_info(response,"Number of reviews")
+
     	yield{
        		'Title': book_name,
        		'Price': price,
        		'URL' : url,
        		'Image': image,
        		'Descrption': descrption,
-       		'Rating':rating
+       		'Rating':rating,
+       		'UPC': UPC,
+       		'Product_Type': Product_Type,
+       		'Price_excl_tax': Price_excl_tax,
+       		'Price_incl_tax': Price_incl_tax,
+       		'Tax': Tax,
+       		'Availability': Availability,
+       		'Number_of_reviews': Number_of_reviews,
        	} 
